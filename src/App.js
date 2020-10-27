e0b3ab433dc079707b77fc3af8b3574c5b302c70
nohsangwoo <nsgr12@naver.com>
2020-10-28 01:09:58 +0900

    #2 Vanilla Todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { createStore } from "redux";
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+const reducer = (state = [], action) => {};
+
+const store = createStore(reducer);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" placeholder="Write to do" />
+        <button>Add</button>
+      </form>
+      <ul></ul>
     </div>
   );
 }
